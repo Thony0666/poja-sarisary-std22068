b5cdcabcc5ff872963cb222da8bf654e930a9631
Thony0666 <160373228+Anthonyo02@users.noreply.github.com>
2024-02-22 12:26:01 +0300

    last commit

--- format.sh
+++ format.sh
@@ -1,3 +1,2 @@
 #!/bin/sh
-
-find src -name "*.java" -exec java -jar google-java-format-1.18.1-all-deps.jar --replace {} \;
+for /R %%i IN (*.java) DO java -jar google-java-format-1.18.1-all-deps.jar --replace %%i
